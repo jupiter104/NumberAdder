fdfdc3df02ee4c79e1cf2adbd2fce1e4e59a0551
Debashis <jupiter_104@ymail.com>
2019-03-28 13:42:04 +0530

    Add files via upload

---
+++ NumberAdder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number to add or type 'ok' to exit");
+
+                string input = Console.ReadLine();
+
+                if (input == "ok")
+                {
+                    break;
+                }
+
+                double number = Convert.ToDouble(input);
+
+                sum += number;
+
+                Console.WriteLine(sum.ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
